82b45a765f11f9421d0c1a182908e684962af006
谢娟敏 <juanmin@staff.weibo.com>
2021-10-28 20:19:00 +0800

    引入VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  title: "Concept",
+  description: "A blog of a front-end engineer",
+  base: '/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '前端',
+        ariaLabel: 'Front End Menu',
+        items: [
+          { text: 'HTML', link: '' },
+          { text: 'CSS', link: '' },
+          { text: 'JavaScript', link: '' },
+          { text: '算法', link: '' },
+          { text: '工程化', link: '' },
+          { text: '可视化', link: '' },
+          { text: '前端安全', link: '' },
+          { text: '前端性能', link: '' }
+        ]
+      },
+      {
+        text: '后端',
+        ariaLabel: 'Back End Menu',
+        items: [
+          { text: 'Node', link: '' },
+          { text: 'PHP', link: '' }
+        ]
+      },
+      {
+        text: '选择语言',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: '简体中文', link: '/language/chinese/' },
+          { text: 'English', link: '/language/English/' }
+        ]
+      },
+      { text: 'Github', link: 'https://github.com/xjmconcept' },
+    ]
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}
